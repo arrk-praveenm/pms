0b2913de53d7eac7948c6c92eca78f3bd5eb6ac6
Jeevan K Reddy <jeevankumar.reddy@arrkgroup.com>
2015-11-06 18:02:25 +0530

    Checkin aganist to Refactoring work for PDF download

--- src/main/java/com/arrkgroup/apps/hr/showReports/ShowReportController.java
+++ src/main/java/com/arrkgroup/apps/hr/showReports/ShowReportController.java
@@ -170,7 +170,7 @@ public class ShowReportController {
 		modelObjectService.findCycleById(Integer.parseInt(cycleID))
 			.getDescription());
 
-		model.addAttribute("assesseinfo", pdfService.getAssesseesAssessorByCycle(Integer.parseInt(assessor_id),Integer.parseInt(cycleID)));
+		model.addAttribute("assesseinfo", showReportService.getAssesseesAssessorByCycle(Integer.parseInt(assessor_id),Integer.parseInt(cycleID)));
 
 
 
@@ -219,7 +219,7 @@ public class ShowReportController {
 		modelObjectService.findCycleById(Integer.parseInt(cycleID))
 			.getDescription());
 
-		model.addAttribute("assesseinfo", pdfService.getAssesseesAssessorByCycle(Integer.parseInt(assessor_id),Integer.parseInt(cycleID)));
+		model.addAttribute("assesseinfo", showReportService.getAssesseesAssessorByCycle(Integer.parseInt(assessor_id),Integer.parseInt(cycleID)));
 
 		// return a view which will be resolved by a pdf view resolver
 		return new ModelAndView("pdfView", "allSections", allSections);
